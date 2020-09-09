935445c429c0d39ece1b85b229d6946dd5f7fe8e
t7yang <t7yang@gmail.com>
2020-09-10 01:25:03 +0800

    build: add output.globals for iife build in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -8,6 +8,7 @@ const commonjs = require('@rollup/plugin-commonjs');
 const resolve = (...args) => path.resolve(__dirname, ...args);
 
 const extensions = ['.js', '.ts'];
+const iifeSharedOutput = { format: 'iife', name: 'rxlize', globals: { rxjs: 'rxjs' } };
 
 module.exports = {
   external: ['rxjs'],
@@ -15,8 +16,16 @@ module.exports = {
   output: [
     { file: resolve(pkg.module), format: 'es', sourcemap: true },
     { file: resolve(pkg.main), format: 'cjs', sourcemap: true },
-    { file: resolve('dist/iife/rxlize.js'), name: 'rxlize', format: 'iife', sourcemap: 'inline' },
-    { file: resolve('dist/iife/rxlize.min.js'), name: 'rxlize', format: 'iife', plugins: [terser()] },
+    {
+      file: resolve('dist/iife/rxlize.js'),
+      ...iifeSharedOutput,
+      sourcemap: 'inline',
+    },
+    {
+      file: resolve('dist/iife/rxlize.min.js'),
+      ...iifeSharedOutput,
+      plugins: [terser()],
+    },
   ],
   plugins: [
     commonjs(),
