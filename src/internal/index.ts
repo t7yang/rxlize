63e53a024f033201b21df99853a69470cbf2324c
t7yang <t7yang@gmail.com>
2021-07-05 22:50:54 +0800

    style: reformat due prettier update

--- src/internal/index.ts
+++ src/internal/index.ts
@@ -4,17 +4,19 @@ import { RxlAsyncState, RxlCache } from '../types';
 
 export const genErrorSubject = (): Subject<unknown> => new Subject<unknown>();
 
-export const genLoadingState = <R>(cache: RxlCache<R>) => (): RxlAsyncState<R | undefined> => ({
-  isLoading: true,
-  error: null,
-  data: cache.data,
-});
+export const genLoadingState =
+  <R>(cache: RxlCache<R>) =>
+  (): RxlAsyncState<R | undefined> => ({
+    isLoading: true,
+    error: null,
+    data: cache.data,
+  });
 
-export const handleUpdate = <R>(cache: RxlCache<R>, error$: Subject<unknown>) => (
-  obs: Observable<R | undefined>,
-): Observable<RxlAsyncState<R | undefined>> =>
-  obs.pipe(
-    tap(data => (cache.data = data)),
-    map(data => ({ isLoading: false, error: null, data })),
-    catchError(error => (error$.next(error), of({ isLoading: false, error, data: cache.data }))),
-  );
+export const handleUpdate =
+  <R>(cache: RxlCache<R>, error$: Subject<unknown>) =>
+  (obs: Observable<R | undefined>): Observable<RxlAsyncState<R | undefined>> =>
+    obs.pipe(
+      tap(data => (cache.data = data)),
+      map(data => ({ isLoading: false, error: null, data })),
+      catchError(error => (error$.next(error), of({ isLoading: false, error, data: cache.data }))),
+    );
